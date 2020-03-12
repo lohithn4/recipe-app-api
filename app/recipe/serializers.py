876f856cc1ef7309aaad217e351c9e34878c2a65
lohith <lohithn4@gmail.com>
2020-03-12 18:45:33 +0530

    Create tag add feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -7,5 +7,5 @@ class TagSerializer(serializers.ModelSerializer):
     """Serializer for tag objects"""
     class Meta:
         model = Tag
-        fields = ('id', 'user', 'name')
+        fields = ('id', 'name')
         read_only_fields = ('id',)
